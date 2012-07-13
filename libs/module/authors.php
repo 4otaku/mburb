b3d07d200d1851bd87ef908bcccc61aecca0c732
Nameless <admin@4otaku.ru>
2012-07-13 18:44:59 +0400

    logs_authors

---
+++ libs/module/authors.php
@@ -0,0 +1,6 @@
+<?php
+
+class Module_Authors extends Module_Abstract_Html
+{
+	protected $css = array('main');
+}
---
+++ libs/module/logs.php
@@ -0,0 +1,20 @@
+<?php
+
+class Module_Logs extends Module_Abstract_Html
+{
+	protected $css = array('main');
+
+	protected function get_data() {
+		$logs = Database::order('order')->get_vector(
+			'strip', array('id', 'title', 'date'), '`order` > 0');
+
+		foreach ($logs as &$log) {
+			$log['date'] = strtotime($log['date']);
+			$log['date'] = $log['date'] > 0 ? date('y/m/d', $log['date']) : '';
+		}
+
+		return array(
+			'logs' => $logs
+		);
+	}
+}
