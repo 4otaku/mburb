e333c66e2fcbe44a498e43f4558b9289262177a7
Nameless <admin@4otaku.ru>
2012-07-16 19:14:12 +0400

    fix

--- libs/module/index.php
+++ libs/module/index.php
@@ -5,6 +5,7 @@ class Module_Index extends Module_Page
 	public function __construct($url) {
 		parent::__construct($url);
 
-		$this->id = Database::order('order', 'asc')->get_field('strip', 'order', 'order > 0');
+		$this->id = Database::order('order', 'asc')
+			->get_field('strip', 'order', '`order` > 0');
 	}
 }
